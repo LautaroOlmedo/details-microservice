f39b8bc4241edbe56cb0d3a2ecf7df6e12d5c72c
lautaroolmedo <lolmedo@arquant.com.ar>
2025-08-08 11:20:38 -0300

    updated readme.md

--- internal/infrastrcuture/rest/handler/reader/serve_http.go
+++ internal/infrastrcuture/rest/handler/reader/serve_http.go
@@ -1,12 +1,10 @@
 package reader
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (handler *ReaderHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received request:", r.Method, r.URL.Path)
 	switch {
 	case r.Method == http.MethodGet:
 		handler.HandlerGetDetailsByID(w, r)
